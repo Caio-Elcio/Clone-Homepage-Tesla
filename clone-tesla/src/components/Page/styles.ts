f214b916471400c5ec83e6a78fdf5f4bb27c35a9
Caio-Elcio <caio.eloy@bandtec.com.br>
2021-11-11 09:26:19 -0300

    Projeto finalizado utilizando ReactJS

---
+++ clone-tesla/src/components/Page/styles.ts
@@ -0,0 +1,65 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  .colored:nth-child(1) {
+    background-color: #f1ffe7;
+    background-image: url(https://images2.alphacoders.com/115/1154167.jpg);
+    width: 100%;
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+  }
+  .colored:nth-child(2) {
+    background-color: #dfffd9;
+    background-image: url(https://directimports.com.br/wp-content/uploads/2021/09/Model-Y.jpg);
+    width: 100%;
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+  }
+  .colored:nth-child(3) {
+    background-color: #cdfeca;
+    background-image: url(https://www.hibridosyelectricos.com/media/hibridos/images/2021/03/26/2021032619052066969.jpg);
+    width: 100%;
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+  }
+  .colored:nth-child(4) {
+    background-color: #bbfebb;
+    background-image: url(https://brasilsuleducacional.com.br/adm/uploads/model-x-social_91e2192390.jpg);
+    width: 100%;
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+  }
+  .colored:nth-child(5) {
+    background-color: #a9fdac;
+    background-image: url(https://tesla-cdn.thron.com/delivery/public/image/tesla/45992f1c-a33a-4a04-b1f0-338aff182f8e/bvlatuR/std/2880x1800/_25-Hero-D);
+    width: 100%;
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+  }
+  .colored:nth-child(6) {
+    background-color: #90f29c;
+    background-image: url(https://www.tesla.com/sites/default/files/solarroof/v3/design/solar-roof_design_D.jpg);
+    width: 100%;
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+  }
+  .colored:nth-child(7) {
+    background-color: #77e68c;
+    background-image: url(https://www.tesla.com/ns_videos/commerce/content/dam/tesla/CAR_ACCESSORIES/MODEL_S/CHARGING_ADAPTERS/1457768-01-F_0.jpg);
+    width: 100%;
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+  }
+`
+
+export const Spacer = styled.div`
+  height: 15vh;
+  /* background-color: #77e68c; */
+`
